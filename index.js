1a7f2e02e2b26653ad1135f4f257c0ff34981b97
teklumezgebo <mezge002@umn.edu>
2023-01-24 15:28:11 -0600

    commit tree 3192fb1d667bbd726ce3afed24524f755f4acce5

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+
+function destructivelyAppendCat(name) {
+  cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift()
+}
+
+function appendCat(name) {
+  let catCat = [...cats, name]
+  return catCat
+}
+
+function prependCat(name) {
+  let catsCat = [name, ...cats]
+  return catsCat
+}
+
+function removeLastCat() {
+  return cats.slice(0,2)
+}
+
+function removeFirstCat() {
+  return cats.slice(1,3)
+}
